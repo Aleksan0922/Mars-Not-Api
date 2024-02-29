5f1752d065870e9f2c6f2da65950f8002e2ba657
Aleksan_09 <platondovgan2006@gmail.com>
2024-02-29 18:05:23 +0300

    uwu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 import datetime
 
 app = Flask(__name__)
@@ -53,7 +54,18 @@ def main():
     user3.email = "email@emil.ru"
     user3.hashed_password = "nagibator228"
     user3.modified_date = datetime.datetime.now()
+
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+
     db_sess = db_session.create_session()
+    db_sess.add(job)
+    db_sess.commit()
     db_sess.add(user)
     db_sess.add(user1)
     db_sess.add(user2)
